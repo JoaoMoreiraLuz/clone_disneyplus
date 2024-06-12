25228e8a666cb4d5b289e7d4b4206f6b724bff44
Joao Gustavo <joaogustavo9992@outlook.com>
2024-06-12 14:02:34 -0300

    criando a sessão de FAQs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
 
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(button) {
@@ -11,7 +12,18 @@ document.addEventListener('DOMContentLoaded', function() {
             button.target.classList.add('shows__tabs__button--active');
         })
     }
+
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', OpenOrCloseAnswer);
+    }
 })
+
+function OpenOrCloseAnswer(element) {
+    const classAdd = 'FAQ__questions__item--open'
+    const FatherFigure = element.target.parentNode
+
+    FatherFigure.classList.toggle(classAdd);
+}
 
 function removeActiveButton() {
     const buttons = document.querySelectorAll('[data-tab-button]');
